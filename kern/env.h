b3ecfcf7bccd2d288bbe7609fb219cac5aa3030a
YaoZengzeng <yaozengzeng@zju.edu.cn>
2017-01-11 11:16:36 +0800

    allocate the environments array

---
+++ kern/env.h
@@ -0,0 +1,9 @@
+#ifndef YUOS_KERN_ENV_H
+#define	YUOS_KERN_ENV_H
+
+#include <inc/env.h>
+
+extern struct Env *envs;		// All environments
+extern struct Env *curenv;		// Current environment
+
+#endif 	/* !YUOS_KERN_ENV_H */
